86fadf2daf16d19cc4f529921b4a07efea00eebf
Mac <mac@MacBook-Pro-Mac.local>
2024-09-06 19:24:50 +0400

    adaptive advantages

--- src/components/Advantages/ui/Advantages.tsx
+++ src/components/Advantages/ui/Advantages.tsx
@@ -1,21 +1,19 @@
-"use client";
-
 import { Container } from "@/shared/ui/container";
 import { AdvantagesScrollSlider } from "./AdvantagesScrollSlider";
 
 const Advantages = () => {
   return (
-    <section className="bg-white pb-[104px] pt-[88px]">
-      <Container className="ml-auto mr-0 max-w-[1440px]">
+    <section className="bg-white pb-[104px] pt-[88px] max-mobile:pb-[64px] max-mobile:pt-[32px]">
+      <Container className="mobile:ml-auto mobile:mr-0 mobile:max-w-[1440px]">
         <h2 className="heading-two">
           Почему выбирают <span className="text-buttonColor">Red</span>Import?
         </h2>
 
-        <div className="pt-[64px]">
+        <div className="pt-[64px] max-mobile:pt-[32px]">
           <AdvantagesScrollSlider />
         </div>
 
-        <p className="pt-[36px] text-[12px] font-light leading-[14px] text-[#505050]">
+        <p className="pt-[36px] text-[13px] font-light leading-[15.5px] text-[#505050] max-mobile:pt-[20px]">
           Параллельный импорт спецтехники без посредников. Лучшие цены,
           качественная продукция и оперативная доставка — вот почему нас
           выбирают!
--- src/components/Advantages/ui/AdvantagesScrollSlider.tsx
+++ src/components/Advantages/ui/AdvantagesScrollSlider.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import { advantagesData } from "../model/mockData";
 import { AdvantagesScrollSlide } from "./AdvantagesScrollSlide";
 import { motion } from "framer-motion";
 import { useScroll } from "@/shared/hooks/useScroll";
+import { useMedia } from "@/shared/hooks/useMedia";
 
 const AdvantagesScrollSlider = () => {
   const { handleScroll, ref } = useScroll();
 
+  const isMobile = useMedia(768);
+
   return (
     <motion.div
-      onScroll={handleScroll}
-      ref={ref}
-      className="custom-scrollbar flex gap-[20px] overflow-x-auto"
+      onScroll={!isMobile ? handleScroll : undefined}
+      ref={!isMobile ? ref : undefined}
+      className="custom-scrollbar flex gap-[20px] max-mobile:flex-col max-mobile:items-center mobile:overflow-x-auto"
     >
       {advantagesData.map((el) => {
         return <AdvantagesScrollSlide key={el.id} item={el} />;
