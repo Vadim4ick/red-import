639a1951cd16cffffb7667eeeb677f74ac1e918d
Mac <mac@MacBook-Pro-Mac.local>
2024-09-07 14:24:39 +0400

    adding modalCatalog

--- src/components/Catalog/ui/CatalogModal.tsx
+++ src/components/Catalog/ui/CatalogModal.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { CatalogModalProviderContext } from "@/shared/providers/catalogModal";
+import { Button } from "@/shared/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/shared/ui/dialog";
+import Image from "next/image";
 import { useContext } from "react";
 
 const CatalogModal = () => {
@@ -15,16 +16,107 @@ const CatalogModal = () => {
     CatalogModalProviderContext,
   );
 
+  if (!selectedItem) {
+    return null;
+  }
+
   return (
     <Dialog open={open} onOpenChange={onClickCatalogModal}>
-      <DialogContent className="h-full max-h-[725px] w-full max-w-[482px] p-[20px]">
-        <DialogHeader>
-          <DialogTitle>{selectedItem?.title}</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent className="flex h-[725px] w-full max-w-[482px] flex-col justify-between rounded-[2px] border-none p-[20px]">
+        <div>
+          <DialogHeader className="flex flex-row gap-[18px] pb-[16px]">
+            <div>
+              <Image
+                src={selectedItem.imgCatalog}
+                alt={selectedItem.title}
+                width={169}
+                height={101}
+                className="max-h-[101px] max-w-[170px] object-cover"
+              />
+            </div>
+
+            <div className="flex flex-col items-start gap-[6px]">
+              <DialogTitle className="heading-three max-mobile:text-[18px] max-mobile:leading-[22px]">
+                {selectedItem.title}
+              </DialogTitle>
+
+              <p className="text-[14px] leading-[16px] text-[#5A5A5A]">
+                {selectedItem.subtitle}
+              </p>
+
+              <div className="flex flex-col items-start gap-[6px] max-mobile:gap-[2px]">
+                <div className="heading-three">
+                  {selectedItem.price}{" "}
+                  <span className="text-[14px] leading-[16px]">руб.</span>
+                </div>
+
+                {selectedItem.nds && (
+                  <div className="text-[12px] leading-[14px] text-[#5A5A5A]">
+                    Цена с НДС {selectedItem.nds}
+                  </div>
+                )}
+              </div>
+            </div>
+          </DialogHeader>
+
+          <div className="rounded-[2px] bg-[#F5F5F5] py-[2px] pl-[20px] pr-[4px]">
+            <div className="custom-scrollbar__catalog h-[469px] overflow-y-scroll pr-[12px]">
+              {/* Виртуальные паддинги сверху */}
+              <div className="sticky top-0 h-[18px] w-full bg-[#F5F5F5]"></div>
+
+              <div className="flex w-full flex-col gap-2">
+                {selectedItem.parameters.map((param) => {
+                  return (
+                    <p
+                      key={param.id}
+                      className="flex gap-2 text-[14px] leading-[16px]"
+                    >
+                      <span className="font-light text-[#7B7B7B]">
+                        {param.text}
+                      </span>
+                      <span>{param.value}</span>
+                    </p>
+                  );
+                })}
+              </div>
+
+              {selectedItem.details && (
+                <div className="text-[14px]">
+                  <p className="pb-[12px] pt-[24px] leading-[16px]">
+                    Дополнительные опции:
+                  </p>
+
+                  <ul className="flex flex-col gap-2">
+                    {selectedItem.details.map((detail, idx) => {
+                      return (
+                        <li
+                          key={idx}
+                          className="text-[14px] font-light leading-[16px] text-[#5A5A5A]"
+                        >
+                          {detail}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              )}
+
+              {/* Виртуальные паддинги снизу */}
+              <div className="sticky bottom-0 h-[18px] w-full bg-[#F5F5F5]"></div>
+            </div>
+          </div>
+        </div>
+
+        <p className="text-center text-[14px] leading-[16px]">
+          Гарантия 1500 м*ч или 18 мес., что наступит раньше.
+        </p>
+
+        <Button
+          variant={"reverseDefault"}
+          className="h-[42px] w-full font-normal"
+        >
+          Купить сейчас
+        </Button>
       </DialogContent>
     </Dialog>
   );
--- src/shared/ui/dialog.tsx
+++ src/shared/ui/dialog.tsx
@@ -5,6 +5,7 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 
 import { cn } from "@/shared/lib/utils";
+import { Close } from "../icons/Close";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -44,8 +45,8 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <Cross2Icon className="h-4 w-4" />
+      <DialogPrimitive.Close className="absolute right-[4px] top-[4px] rounded-sm bg-[#F5F5F5] p-[7px] outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+        <Close />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
