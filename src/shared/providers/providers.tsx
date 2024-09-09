d968a271d30e16a56445907f2606de587df8e975
Mac <mac@MacBook-Pro-Mac.local>
2024-09-09 22:24:31 +0400

    add loader

--- src/shared/providers/providers.tsx
+++ src/shared/providers/providers.tsx
@@ -2,6 +2,8 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CatalogModalProvider } from "./catalogModal";
+import { useEffect, useState } from "react";
+import { Loader } from "../ui/loader";
 
 // Create a client
 const queryClient = new QueryClient({
@@ -13,6 +15,16 @@ const queryClient = new QueryClient({
 });
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Loader className="absolute left-1/2 top-1/2 size-10" />;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <CatalogModalProvider>{children}</CatalogModalProvider>;
