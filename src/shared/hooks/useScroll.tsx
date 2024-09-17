6ead11a0e0484360c9bb35e642bdcc908edb0028
Mac <mac@MacBook-Pro-Mac.local>
2024-09-17 18:11:43 +0400

    fix scroll

--- src/shared/hooks/useScroll.tsx
+++ src/shared/hooks/useScroll.tsx
@@ -1,3 +1,86 @@
+// "use client";
+
+// import { useInView } from "framer-motion";
+// import { useEffect, useRef, useState } from "react";
+
+// const useScroll = () => {
+//   const sliderRef = useRef<HTMLDivElement | null>(null);
+
+//   const [isScrollLocked, setIsScrollLocked] = useState(true); // Заблокирован (Да, нет)
+//   const [hasScrolled, setHasScrolled] = useState(false); // Проскроллен (Да, нет)
+
+//   const isInView = useInView(sliderRef, {
+//     amount: "all",
+//   });
+
+//   useEffect(() => {
+//     if (hasScrolled) {
+//       sliderRef.current = null;
+//     }
+//   }, [hasScrolled]);
+
+//   useEffect(() => {
+//     const handleWheel = (e: WheelEvent) => {
+//       if (isScrollLocked && !hasScrolled && sliderRef.current && isInView) {
+//         sliderRef.current.scrollLeft += e.deltaY;
+//       }
+//     };
+
+//     if (isScrollLocked && !hasScrolled && isInView) {
+//       document.body.style.overflow = "hidden";
+//       window.addEventListener("wheel", handleWheel);
+//     } else {
+//       document.body.style.overflow = "auto";
+//     }
+
+//     return () => {
+//       document.body.style.overflow = "auto";
+//       window.removeEventListener("wheel", handleWheel);
+//     };
+//   }, [isScrollLocked, isInView, hasScrolled]);
+
+//   // Проверка, находится ли элемент вне видимой области при загрузке
+//   useEffect(() => {
+//     const rect = sliderRef.current?.getBoundingClientRect();
+
+//     if (!rect) return;
+
+//     if (rect.y < 0) {
+//       setHasScrolled(true);
+//     }
+//   }, []);
+
+//   const handleScroll = () => {
+//     if (!sliderRef.current) return;
+
+//     const { scrollLeft, scrollWidth, clientWidth } = sliderRef.current;
+
+//     if (scrollLeft === 0 || scrollLeft + clientWidth >= scrollWidth) {
+//       setIsScrollLocked(false);
+//       document.body.style.overflow = "auto";
+//       setHasScrolled(true);
+
+//       if (scrollLeft === 0) {
+//         setIsScrollLocked(true);
+//       }
+
+//       if (scrollLeft + clientWidth < scrollWidth) {
+//         setHasScrolled(false);
+//       }
+//     } else {
+//       setIsScrollLocked(true);
+//       document.body.style.overflow = "hidden";
+//     }
+//   };
+
+//   return {
+//     ref: sliderRef,
+//     handleScroll,
+//   };
+// };
+
+// export { useScroll };
+
 "use client";
 
 import { useInView } from "framer-motion";
@@ -6,8 +89,8 @@ import { useEffect, useRef, useState } from "react";
 const useScroll = () => {
   const sliderRef = useRef<HTMLDivElement | null>(null);
 
-  const [isScrollLocked, setIsScrollLocked] = useState(true); // Заблокирован (Да, нет)
-  const [hasScrolled, setHasScrolled] = useState(false); // Проскроллен (Да, нет)
+  const [isScrollLocked, setIsScrollLocked] = useState(true);
+  const [hasScrolled, setHasScrolled] = useState(false);
 
   const isInView = useInView(sliderRef, {
     amount: "all",
@@ -27,19 +110,25 @@ const useScroll = () => {
     };
 
     if (isScrollLocked && !hasScrolled && isInView) {
+      // Lock scroll and adjust padding
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
       document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
       window.addEventListener("wheel", handleWheel);
     } else {
+      // Unlock scroll and reset padding
       document.body.style.overflow = "auto";
+      document.body.style.paddingRight = "0";
     }
 
     return () => {
       document.body.style.overflow = "auto";
+      document.body.style.paddingRight = "0";
       window.removeEventListener("wheel", handleWheel);
     };
   }, [isScrollLocked, isInView, hasScrolled]);
 
-  // Проверка, находится ли элемент вне видимой области при загрузке
   useEffect(() => {
     const rect = sliderRef.current?.getBoundingClientRect();
 
@@ -54,11 +143,8 @@ const useScroll = () => {
     if (!sliderRef.current) return;
 
     const { scrollLeft, scrollWidth, clientWidth } = sliderRef.current;
-
     const scrollbarWidth =
       window.innerWidth - document.documentElement.clientWidth;
-
-    console.log(scrollbarWidth);
 
     if (scrollLeft === 0 || scrollLeft + clientWidth >= scrollWidth) {
       setIsScrollLocked(false);
@@ -77,7 +163,7 @@ const useScroll = () => {
     } else {
       setIsScrollLocked(true);
       document.body.style.overflow = "hidden";
-      document.body.style.paddingRight = `${scrollbarWidth}px`; // Установить padding при блокировке
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
     }
   };
 
