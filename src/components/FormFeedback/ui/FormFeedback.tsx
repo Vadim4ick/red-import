42e438922183e8b2be6c7e318e54518494e69bb2
Mac <mac@MacBook-Pro-Mac.local>
2024-10-04 11:40:13 +0400

    fix animate

--- src/components/FormFeedback/ui/FormFeedback.tsx
+++ src/components/FormFeedback/ui/FormFeedback.tsx
@@ -12,45 +12,46 @@ const FormFeedback = () => {
   const { data, isLoading } = useGetSectionFormSendQuery();
 
   return (
-    <motion.section
-      ref={ref}
-      {...motionConfigAnimate}
-      animate={
-        inView ? motionConfigAnimate.animate : motionConfigAnimate.initial
-      }
-      className="rounded-[2px] bg-white mobile:pb-[128px]"
-    >
+    <section className="rounded-[2px] bg-white mobile:pb-[128px]">
       <Container className="max-w-[1258px] max-mobile:px-0">
-        <div className="flex justify-between gap-2 bg-[#EDEDED] pb-[28px] pl-[68px] pr-[98px] pt-[44px] max-desktop:px-[45px] max-mobile:flex-col max-mobile:items-center max-mobile:gap-[24px] max-mobile:px-[20px] max-mobile:pb-[64px] max-mobile:pt-[32px]">
-          <div className="flex max-w-[596px] flex-col justify-between gap-[36px]">
-            <div className="flex flex-col gap-[24px] max-mobile:gap-[48px]">
-              {data?.sectionFormSend && (
-                <h2 className="heading-two max-mobile:max-w-[246px]">
-                  {data.sectionFormSend.title}
-                </h2>
-              )}
+        <motion.div
+          ref={ref}
+          {...motionConfigAnimate}
+          animate={
+            inView ? motionConfigAnimate.animate : motionConfigAnimate.initial
+          }
+        >
+          <div className="flex justify-between gap-2 bg-[#EDEDED] pb-[28px] pl-[68px] pr-[98px] pt-[44px] max-desktop:px-[45px] max-mobile:flex-col max-mobile:items-center max-mobile:gap-[24px] max-mobile:px-[20px] max-mobile:pb-[64px] max-mobile:pt-[32px]">
+            <div className="flex max-w-[596px] flex-col justify-between gap-[36px]">
+              <div className="flex flex-col gap-[24px] max-mobile:gap-[48px]">
+                {data?.sectionFormSend && (
+                  <h2 className="heading-two max-mobile:max-w-[246px]">
+                    {data.sectionFormSend.title}
+                  </h2>
+                )}
 
-              {data?.sectionFormSend && (
-                <p className="max-w-[491px] leading-[19px] text-[#5A5A5A] max-tablet:hidden">
-                  {data.sectionFormSend.description}
-                </p>
-              )}
+                {data?.sectionFormSend && (
+                  <p className="max-w-[491px] leading-[19px] text-[#5A5A5A] max-tablet:hidden">
+                    {data.sectionFormSend.description}
+                  </p>
+                )}
+              </div>
+
+              <div className="max-mobileSmall:flex max-mobileSmall:justify-center">
+                <Image
+                  src={"/img/formFeedback/1.png"}
+                  alt="img"
+                  width={322}
+                  height={197}
+                />
+              </div>
             </div>
 
-            <div className="max-mobileSmall:flex max-mobileSmall:justify-center">
-              <Image
-                src={"/img/formFeedback/1.png"}
-                alt="img"
-                width={322}
-                height={197}
-              />
-            </div>
+            <FormFeedbackForm />
           </div>
-
-          <FormFeedbackForm />
-        </div>
+        </motion.div>
       </Container>
-    </motion.section>
+    </section>
   );
 };
 
